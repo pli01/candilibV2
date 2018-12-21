d56583209345ba0b80f85884cebbf157f93c96dc
Stanislas Ormières <stan@stormier.ninja>
2018-12-21 18:20:27 +0100

    Add essential middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import compression from 'compression'
+import bodyParser from 'body-parser'
 import morgan from 'morgan'
 
 import { loggerStream } from './logger'
@@ -6,6 +8,10 @@ import { loggerStream } from './logger'
 const app = express()
 
 app.use(morgan('combined', { stream: loggerStream }))
+
+app.use(compression())
+app.use(bodyParser.json({ limit: '20mb' }))
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
